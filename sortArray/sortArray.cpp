a454a7e3aaeb76c3789063ad95b40ac367f39932
42Miles <aistra200050@gmail.com>
2021-05-09 13:05:00 +0200

    Optimized bubble sort

--- sortArray/sortArray.cpp
+++ sortArray/sortArray.cpp
@@ -7,15 +7,29 @@ int main()
 	const int length(9);
 	int array[length] = { 7, 5, 6, 4, 9, 8, 2, 1, 3 };
 
+
 	for (int x = 0; x < length - 1; ++x)
 	{
-		for (int index = 0; index < length - 1; ++index)
-		{
-			if (array[index] > array[index + 1])
-				std::swap(array[index], array[index + 1]);
-		}
-	}
+		int endOfIteration(length - x);
 
+		bool swapped = false;
+
+		for (int index = 0; index < endOfIteration - 1; ++index)
+		{
+			if (array[index] > array[index + 1]) 
+			{
+				std::swap(array[index], array[index + 1]);
+				swapped = true;
+			}
+		}
+
+		if (!swapped)
+		{
+			std::cout << "Early termination on iteration: " << x + 1 << std::endl;
+			break;
+		}
+
+	}
 	for (int i = 0; i < length; ++i)
 		std::cout << array[i] << " ";
 	
